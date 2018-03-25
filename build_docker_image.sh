042279b5c1be2780400b83e20374414de28c885a
shihezichen <shihezichen@live.cn>
2018-03-25 22:24:03 +0800

    Update build_docker_image.sh

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-# function: build a slam evnironmen docker images
+# function: build a slam evnironmen docker image
 # author: shihezichen@live.cn
 
+echo ""
+echo "This program will build the slam environment docker image with the Dockefile."
+echo ""
 
 if [ ! -f "./Dockerfile" ]; then
-	echo "Can't find the Dockerfile. exit with 1"
+	echo "Can't find the Dockerfile at the current directory. Exit with 1"
 	exit 1
 fi
 
@@ -30,9 +33,9 @@ sudo docker images
 
 echo ""
 echo "- start docker container shihezichen/slam_ubuntu16.04_desktop_env "
-echo "docker run -it --rm -p 5900:5900  \"
-echo "      -e VNC_PASSWORD=hadoop -e RESOLUTION=1920x1080  \"
-echo "      -e USER=hadoop -e PASSWORD=hadoop  \"
+echo "docker run -it --rm -p 5900:5900 \\  "
+echo "      -e VNC_PASSWORD=hadoop -e RESOLUTION=1920x1080 \\ "
+echo "      -e USER=hadoop -e PASSWORD=hadoop  \\  "
 echo "      shihezichen/slam_ubuntu16.04_desktop_env"
 sudo docker run -it --rm -p 5900:5900  \
       -e VNC_PASSWORD=hadoop -e RESOLUTION=1920x1080  \
