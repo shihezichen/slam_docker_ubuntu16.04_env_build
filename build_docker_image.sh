3d3c26abc7abde9da6a3db0494d6ae329a070699
shihezichen <shihezichen@live.cn>
2018-03-25 08:54:40 +0800

    Update build_docker_image.sh

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# function: build a slam evnironmen docker images and run it
+# function: build a slam evnironmen docker images
 # author: shihezichen@live.cn
 
 
@@ -9,6 +9,7 @@ if [ ! -f "./Dockerfile" ]; then
 	exit 1
 fi
 
+echo ""
 echo "- docker build -t shihezichen/slam_ubuntu16.04_desktop_env  ./"
 echo ""
 sudo docker build -t shihezichen/slam_ubuntu16.04_desktop_env  ./
@@ -16,17 +17,26 @@ if [ $? -ne 0 ]; then
 	echo "FAILED. exit with 1"
 	exit 1
 else
-	echo ""
+    echo ""
 	echo "done."
 fi
 
+echo ""
 echo "- docker images"
+echo ""
 sudo docker images
 
 
-echo "- run docker container shihezichen/slam_ubuntu16.04_desktop_env "
+
+echo ""
+echo "- start docker container shihezichen/slam_ubuntu16.04_desktop_env "
+echo "docker run -it --rm -p 5900:5900  \"
+echo "      -e VNC_PASSWORD=hadoop -e RESOLUTION=1920x1080  \"
+echo "      -e USER=hadoop -e PASSWORD=hadoop  \"
+echo "      shihezichen/slam_ubuntu16.04_desktop_env"
 sudo docker run -it --rm -p 5900:5900  \
       -e VNC_PASSWORD=hadoop -e RESOLUTION=1920x1080  \
       -e USER=hadoop -e PASSWORD=hadoop  \
       shihezichen/slam_ubuntu16.04_desktop_env
+
 
